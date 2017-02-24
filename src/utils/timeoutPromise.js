001506c563865820494e62c798ce3f3a99a570aa
Vaclav Mikolasek <vaclav.mikolasek@gmail.com>
2017-02-24 17:21:07 +0100

    Minor renaming

--- src/utils/timeoutPromise.js
+++ src/utils/timeoutPromise.js
@@ -4,11 +4,11 @@
  * unless the `racedPromise` resolves first. In that case it resolves to the value of `racedPromise`
  */
 export default function timeoutPromise(racedPromise, milliseconds, valueAfterTimeout) {
-    const timeoutPromise = new Promise((resolve) => {
+    const racingPromise = new Promise((resolve) => {
         window.setTimeout(() => resolve(valueAfterTimeout), milliseconds)
     })
     return Promise.race([
         racedPromise,
-        timeoutPromise,
+        racingPromise,
     ])
 }
